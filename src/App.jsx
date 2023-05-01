cb33b7d2d5b5f2286e444e878bf3922284c28dee
Sushipie <ahmad.thakur@outlook.com>
2023-05-01 19:03:23 +0500

    Refactored code

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,42 @@
+import { useState } from "react";
 import "./App.css";
-import kaomojis from "./assets/kaomoji";
+import kaomoji from "./assets/kaomoji";
 import KaomojiCard from "./components/KaomojiCard";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Button from "./components/Button";
 
 function createCard(kaomoji) {
-  return (
-    <KaomojiCard
-      key={kaomoji.id}
-      kaomoji={kaomoji.face}
-      meaning={kaomoji.description}
-    />
-  );
+  return <KaomojiCard kaomoji={kaomoji} />;
 }
 
 function App() {
+  const [category, setCategory] = useState(kaomoji[0].list);
+
+  function handleClick(event) {
+    let categoryName = event.target.innerText;
+    console.log(categoryName);
+
+    //in the kaomoji.js, find the obj with the same name as the button
+    const obj = kaomoji.find((item) => item.name == categoryName);
+    console.log(obj);
+    setCategory(obj.list);
+  }
+
   return (
     <>
-      <h1 className="title">100 Kaomojies</h1>
-      <h2 className="subtitle">Click to copy</h2>
-      <div className="collection">{kaomojis.map(createCard)}</div>
+      <Header />
+      <div className="button-container">
+        {kaomoji.map((category) => {
+          return (
+            <>
+              <Button onClick={handleClick} text={category.name} />
+            </>
+          );
+        })}
+      </div>
+      <div className="card-collection">{category.map(createCard)}</div>
+      <Footer author={"Sushipie"} />
     </>
   );
 }
